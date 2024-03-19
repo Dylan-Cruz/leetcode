ce35e8319c26c6d3476706bfe198e9ee25776f48
dylan <9627014+Dylan-Cruz@users.noreply.github.com>
2024-03-18 21:32:50 -0400

    preliminary solution for 20 valid parentheses

--- 20_valid_parentheses/solution.py
+++ 20_valid_parentheses/solution.py
@@ -3,5 +3,31 @@ from collections import deque
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        # ({}[({})])
-        pass
+
+        # if there are an uneven number of characters, return false
+        if len(s) % 2 != 0:
+            return False
+
+        terminating_map = {')': '(', ']': '[', '}': '{'}
+        
+        queue = deque()
+
+        for char in s:
+            if char in terminating_map.keys:
+                # if the top_val is the expected opening paren
+                # pop it off the queue
+                top_val = deque[0]
+                if terminating_map[char] == top_val:
+                    deque.popleft()
+                else: 
+                    # an invalid opening parentheses 
+                    return False
+            else:
+                queue.append(char)
+        
+        if not queue:
+            # if queue isn't empty
+            return False
+
+        return True
+        
