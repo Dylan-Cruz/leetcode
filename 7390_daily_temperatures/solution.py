378266a47e69fc4d48f8395b1ea86a8100b31dc4
Dylan Cruz <9627014+Dylan-Cruz@users.noreply.github.com>
2024-04-21 21:50:40 -0400

    pseudo code for 7390

--- 7390_daily_temperatures/solution.py
+++ 7390_daily_temperatures/solution.py
@@ -1,3 +1,7 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
+        # use a monotonic decreasing stack 
+        # if the next value in the array is greater than the value on the top of the stck
+        # set the associated value in the arra to the difference.
+        # otherwise push it on the stack as well
+        # at the end of the array, push 0 to any remaining associated indexes
