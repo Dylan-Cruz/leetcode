32eb2a122c67e6e2e4c53546a9b6b0101f1c3597
dylan <9627014+Dylan-Cruz@users.noreply.github.com>
2024-04-27 21:09:25 -0400

    fix typing import

--- 7390_daily_temperatures/solution.py
+++ 7390_daily_temperatures/solution.py
@@ -1,6 +1,10 @@
+from typing import List
+
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # use a monotonic decreasing stack 
+        
         # if the next value in the array is greater than the value on the top of the stck
         # set the associated value in the arra to the difference.
         # otherwise push it on the stack as well
