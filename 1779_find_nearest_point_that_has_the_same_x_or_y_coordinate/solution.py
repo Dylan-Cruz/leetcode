9d919ecd458f0ac1cd89b728c59db3e69e8e1443
Dylan Cruz <9627014+Dylan-Cruz@users.noreply.github.com>
2024-05-28 15:22:28 -0400

    1779 solved

--- 1779_find_nearest_point_that_has_the_same_x_or_y_coordinate/solution.py
+++ 1779_find_nearest_point_that_has_the_same_x_or_y_coordinate/solution.py
@@ -1,3 +1,19 @@
+from typing import List
+
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+        result = -1
+        min_dist = float('inf')
+        for i, point in enumerate(points):
+            if point[0] == x or point[1] == y:
+                dist = abs(x - point[0]) + abs(y - point[1])
+                if dist < min_dist:
+                    result = i
+                    min_dist = dist
         
+        return result
+    
+solution = Solution()
+print(solution.nearestValidPoint(3, 4, [[1,2],[3,1],[2,4],[2,3],[4,4]]))
+print(solution.nearestValidPoint(3, 4, [[3,4]]))
+print(solution.nearestValidPoint(3, 4, [[2,3]]))
