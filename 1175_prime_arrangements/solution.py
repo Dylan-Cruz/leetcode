a7c21db151529759e7b05d3676da3c0eb18814b0
Dylan Cruz <9627014+Dylan-Cruz@users.noreply.github.com>
2024-05-31 21:32:39 -0400

    /bin/python3 /home/dylan/Code/leetcode/1175_prime_arrangements/solution.py

--- 1175_prime_arrangements/solution.py
+++ 1175_prime_arrangements/solution.py
@@ -1,6 +1,30 @@
+import math
+
+
 class Solution:
     def numPrimeArrangements(self, n: int) -> int:
-        # count the number of prime numbers 2 to n
-        # there are the same number of prime indexes in a 1 indexed list 
-        # accumulate n! as we encounter primes
+        encountered_primes = 0
+
+        for i in range(2, n+1):
+            if Solution.isPrime(i):
+                encountered_primes += 1
+
+        return math.factorial(n - encountered_primes) * math.factorial(encountered_primes) % (10**9+7)
+
+    @staticmethod
+    def isPrime(n):
+        if n < 2:
+            return False
+        if n == 2: 
+            return True
+        if n % 2 == 0:
+            return False
         
+        for i in range(3, math.ceil(n**0.5) +1, 2):
+            if n % i == 0:
+                return False
+        
+        return True
+
+solution = Solution()
+print(solution.numPrimeArrangements(100))
--- 1175_prime_arrangements/write_up.md
+++ 1175_prime_arrangements/write_up.md
@@ -2,6 +2,12 @@
 
 ## Approach
 
+My initial instinct was pretty close on this one. It was to count the number of primes in the range (NP) and return NP!. The only problem is, that only generates the permutations of prime numbers in prime indices. It doesn't consider the arrangement of nonprime numbers. To include those, we multiply NP! by C! where C is the number of composite numbers
+
 ## Time Complexity
 
+O(N^2) since there's an inner loop in isPrime. This however is bound by a function that can be calculated
+
 ## Space Complexity
+
+O(1) this is done in constant space
