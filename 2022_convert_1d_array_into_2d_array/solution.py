82814145b7c129261f4b373214db1abe63fa6a44
Dylan Cruz <9627014+Dylan-Cruz@users.noreply.github.com>
2024-06-04 09:25:38 -0400

    1d to 2d array

--- 2022_convert_1d_array_into_2d_array/solution.py
+++ 2022_convert_1d_array_into_2d_array/solution.py
@@ -1,3 +1,32 @@
+from typing import List
+
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m*n != len(original):
+            return []
         
+        rows = []
+        col = []
+        rowNum = 0
+        for value in original:
+            col.append(value)
+            if len(col) == n:
+                rows.append(col)
+                col = []
+
+        return rows
+    
+    def betterConstruct2DArray(self, original: List[int], m: int, n:int) -> List[List[int]]:
+        array_2d = []
+        if len(original) == m * n:
+            for ind_row in range(m):
+                array_2d.append(original[ind_row*n : ind_row*n + n])
+
+        return array_2d      
+
+    
+solution = Solution()
+print(solution.construct2DArray([6,2,6,1,8,9,1,2,6], 3, 3))
+print(solution.construct2DArray([6,2,6,1,8,9,1,2], 3, 3))
+print(solution.construct2DArray([1,2,3,4,5], 2, 2))
+print(solution.construct2DArray([6,2,6], 1,3))
--- 2022_convert_1d_array_into_2d_array/write_up.md
+++ 2022_convert_1d_array_into_2d_array/write_up.md
@@ -2,6 +2,12 @@
 
 ## Approach
 
+This one is easy enough although it'd be nice if python had a built in 2d array like Java. My initial and less efficient approach was to iterate over the list appending to a running column. If the running column is length N long, append the column to the rows list and make a new column. I forgot that python has array slicing, so there's a faster solution where you iterate over the numbers from 0 to m-1 and append a calculated slice from the orignal list.
+
 ## Time Complexity
 
+Both run in O(N) time but the slicing method is faster as there's no check to see if we need to append and clear the column.
+
 ## Space Complexity
+
+O(1) space since the return value doesn't count towards space complexity
