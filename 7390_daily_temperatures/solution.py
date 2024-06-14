9caa041b35b7a6dcf777de77fbd7c3e896f2e9ea
Dylan Cruz <9627014+Dylan-Cruz@users.noreply.github.com>
2024-06-13 22:34:11 -0600

    7390 in progress

--- 7390_daily_temperatures/solution.py
+++ 7390_daily_temperatures/solution.py
@@ -1,11 +1,23 @@
 from typing import List
+from collections import deque
 
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # use a monotonic decreasing stack 
+        ret = [0] * len(temperatures)
+        stack = deque()
+
+        for index, value in enumerate(temperatures[::-1]):
+            if len(stack) == 0:
+                stack.append([index, temperatures[index]])
+            else:   
+                top = stack[-1]
+                if top[1] > temperatures[index]:
+                    
+
         
         # if the next value in the array is greater than the value on the top of the stck
         # set the associated value in the arra to the difference.
         # otherwise push it on the stack as well
         # at the end of the array, push 0 to any remaining associated indexes
+
